d8c9463e069058621e01f745329807fe43e3f750
Talha <talha.aziz@sjsu.edu>
2025-09-05 10:37:24 -0700

    add text box

--- avatar.py
+++ avatar.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas, Text, Entry
+from tkinter import Tk, Canvas, Text
 from PIL import Image, ImageTk
 
 
@@ -7,32 +7,37 @@ root = Tk()
 root.title("assistant")
 
 try:
+    ratio = 4  # controls how much of the screen the avatar takes up
+
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    # create the normal and stretched avatars
+    # create the avatar
     image = Image.open("smile.png")
-
-    ratio = 4
     avatar_width = int(screen_width / ratio)
     avatar_height = int(image.height * (avatar_width / image.width))
-
     image.resize((avatar_width, avatar_height))
     photo_image = ImageTk.PhotoImage(image)
 
     # render the avatar
-    canvas = Canvas(root, bg="black", width=avatar_width, height=avatar_height)
+    canvas = Canvas(root, bg="black")
     canvas.config(highlightthickness=0)
     normal = canvas.create_image(0, 0, image=photo_image, anchor="nw")
     canvas.pack(side="bottom", fill="both", expand=True)
 
-    x = int(screen_width - avatar_width)
-    y = int(screen_height - avatar_height)
+    # render the text box above the avatar
+    text = Text(canvas, bg="white", fg="black", width=30, height=1, font=("Arial", 12), wrap="word")
+    text.config(highlightthickness=0)
+    text.insert("end", "")
+    text.pack(side="top", anchor="nw")
 
     # hides the title bar of the avatar window and ensures it is always on top of other applications
     root.overrideredirect(True)
     root.attributes('-topmost', True)
     root.focus_force()
+
+    x = int(screen_width - avatar_width)
+    y = int(screen_height - avatar_height)
 
     root.geometry(f"{avatar_width}x{avatar_height}+{x}+{y}")
     # root.wm_attributes("-transparent", "black")
