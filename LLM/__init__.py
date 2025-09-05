36b8d2f919042d17e8fe54c524884c445c9214b6
Le Duy Nguyen <leduy.nguyen@sjsu.edu>
2025-09-05 14:00:17 -0700

    Add tool to go to url and automatically do google search if model cannot answer

--- LLM/__init__.py
+++ LLM/__init__.py
@@ -1,14 +1,18 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
-from LLM.tools import open_app, close_app
+from LLM.tools import open_app, close_app, go_to_url, google_search
 
 model = ChatOllama(model="qwen3:8b", reasoning=False, top_p=0.8, temperature=0.7, top_k=20, min_p=0)
 
-tools = [open_app, close_app]
+tools = [open_app, close_app, go_to_url, google_search]
 
 agent_executor = create_react_agent(model, tools)
 
 sys_msg = SystemMessage(content=("You have tools: open_app, close_app"
-                                 "When the user asks to open/close apps, you MUST call the appropriate tool."
-                                 "If there is no tool to be called, you can answer the question."))
+                                 "When the user asks to open/close apps, go to a website, google_search you MUST call the appropriate tool."
+                                 "If there is no tool to be called, you can answer the question."
+                                 "If user type in chrome, you should interpret it as Google Chrome."
+                                 "If user type in edge, you should interpret it as Microsoft Edge"
+                                 "When you cannot answer something without access to the Internet, call the tool google_search and paste the user's question into that function")
+                        )
--- LLM/tools.py
+++ LLM/tools.py
@@ -1,19 +1,22 @@
 from langchain.tools import tool
 import subprocess
-
+import webbrowser
 @tool
 def open_app(app_name: str) -> str:
     """Useful for opening the application the user want to open"""
     from sys import platform
-    if platform == "linux" or platform == "linux2":
-        # linux
-        subprocess.run(app_name)
-    elif platform == "darwin":
-        # MAC OS
-        subprocess.run(["open", "-a", app_name])
-    elif platform == "win32":
-        # Windows
-        subprocess.run(["cmd", "/c", "start", "", app_name], shell=True)
+    try:
+        if platform == "linux" or platform == "linux2":
+            # linux
+            subprocess.run(app_name)
+        elif platform == "darwin":
+            # MAC OS
+            subprocess.run(["open", "-a", app_name])
+        elif platform == "win32":
+            # Windows
+            subprocess.run(["cmd", "/c", "start", "", app_name], shell=True)
+    except Exception as e:
+        return f"Error: {e}"
     return f"{app_name} is opened."
 
 @tool
@@ -29,3 +32,19 @@ def close_app(app_name: str) -> str:
         subprocess.run(["powershell", "-Command", f"Stop-Process -Name {app_name} -Force"], shell = True)
 
     return f"{app_name} is closed."
+
+@tool
+def go_to_url(location: str) -> str:
+    """Open a URL in the system default browser"""
+    try:
+        webbrowser.open(location)
+        return f"{location} is opened in your browser."
+    except Exception:
+        return f"Cannot go to {location}."
+
+@tool
+def google_search(query: str) -> str:
+    """Search on Google the content that is given in query, use this when you need to access the Internet to access information"""
+    url = f"https://www.google.com/search?q={query}"
+    webbrowser.open(url)
+    return f"Google search for {query} is opened in your browser."
