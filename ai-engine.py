158994d524e9a2b8f36995839b1eb025da386593
Le Duy Nguyen <leduy.nguyen@sjsu.edu>
2025-09-05 11:00:01 -0700

    Second iteration of ai-engine, extract response message better

--- ai-engine.py
+++ ai-engine.py
@@ -23,11 +23,27 @@ print("User: ", end="")
 user_input = input().strip()
 human_msg = HumanMessage(content=user_input)
 
+last_message = None
+tool_called = False
+tool_message = None
 for event in agent_executor.stream({"messages": [sys_msg, human_msg]}):
+    # Check to see if tool is called
     if "tools" in event:
-        print(event['tools']['messages'][0].content)
+        tool_called = True
+        # Store the message from the tool
+        tool_message = event['tools']['messages'][0].content
     else:
         for _, payload in event.items():
-            for msg in payload['messages']:
-                print(msg)
+            # Take the messages from the AI agent, if no answer generated, return empty list
+            for msg in payload.get('messages',[]):
+                # Only extract the final message from the AI agent, discard the thingking process
+                last_message = msg.content
+# If tool is called, print the message from tool
+if tool_called:
+    print(tool_message)
+# Check if AI agent generate a response
+elif last_message:
+    print(last_message)
+else:
+    print("Sorry, I couldn't produce a response.")
 
